0d1e72d46a0e509c878206940389fb3740d011ab
ZingLix <me@zinglix.xyz>
2019-03-25 20:03:45 +0800

    init

---
+++ src/main/java/xyz/zinglix/freshfoodstore/FreshfoodstoreApplication.java
@@ -0,0 +1,13 @@
+package xyz.zinglix.freshfoodstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshfoodstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshfoodstoreApplication.class, args);
+    }
+
+}
